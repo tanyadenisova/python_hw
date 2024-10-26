6e83379db5ee67878720e2bb68f818627b82968d
tanyadenisova <tanya.denisova.2001@bk.ru>
2024-10-26 11:55:30 +0300

    lesson_01

---
+++ lesson_01/lesson_1_task_1.py
@@ -0,0 +1,2 @@
+my_name = "tanya"
+print (my_name)
---
+++ lesson_01/lesson_1_task_5.py
@@ -0,0 +1,13 @@
+def phone (num):
+    print(num, end ='')
+phone (8)
+phone (8)
+phone (0)
+phone (0)
+phone (5)
+phone (5)
+phone (5)
+phone (3)
+phone (5)
+phone (3)
+phone (5)
